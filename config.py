53ed974dfd8a37cf16d7e04ed5f152f179366401
Pablo III <pablo.iii@outlook.com>
2019-01-02 14:39:18 -0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+SECRET_KEY = '$@*rxtcyvubnjmkOIUYT$$#@'
+DEBUG = True # APENAS SE VC QUISER ESSA FUNÇÃO, SE NÃO; "False"
+HOST = '127.0.0.1' # APENAS SE ESTIVER USANDO LOCAL, SE NÃO; COMENTA ESSA LINHA
+PORT = 5000
+
+MONGODB_DB = 'grp_prazo'
+MONGODB_HOST = 'ds053295.mlab.com'
+MONGODB_PORT = 53295
+MONGODB_USERNAME = 'admin'
+MONGODB_PASSWORD = "PabloPizette1'"
+
+# mongodb://<dbuser>:<dbpassword>@ds053295.mlab.com:53295/grp_prazo
---
+++ convert_api.py
@@ -0,0 +1,4 @@
+import pdftables_api
+
+c = pdftables_api.Client('5362bhz7xxdj')
+c.csv("/home/pablo/documents/jobs/grp_prazo/2018-12-27 Grupo Prazo/1 - Dados Originais/CFB_1988.pdf", 'CFB_1988')  # replace c.xlsx with c.csv to convert to CSV
